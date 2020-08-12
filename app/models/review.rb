9f273a1e2763a5b056f50b09b5fea11353fa5398
Dorothy Yau <dkyyau@gmail.com>
2020-08-12 12:44:16 +0100

    Added model validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: 0..5, numericality: true
+  validates :restaurant_id, presence: true
 end
