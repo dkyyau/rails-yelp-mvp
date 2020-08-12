88308893b73248fd3a319b86b5c712027c978bbd
Dorothy Yau <dkyyau@gmail.com>
2020-08-12 13:09:24 +0100

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+Faker::Config.locale = "en-GB"
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.unique.name,
+    address: Faker::Address.unique.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save
+end
